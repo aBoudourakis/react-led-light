75e5aa7b497ab9c8906b5bd3d1e2cad99c5c9bb4
Athos <a.boudourakis@outlook.com>
2024-07-12 01:56:09 +0300

    - add glow and shine feature - refactor LedLight.tsx to handle all colors as values provided by usage - refactor LedLight.css to handle new features and changes - update tests to cover new features and changes - change version to 0.3.0

--- src/LedLight.test.tsx
+++ src/LedLight.test.tsx
@@ -4,28 +4,72 @@ import LedLight from './LedLight';
 import '@testing-library/jest-dom';
 
 describe('LedLight Component', () => {
-    test('renders with default color', () => {
-        const { container } = render(<LedLight />);
-        expect(container.firstChild).toHaveClass('Led--orange');
+    it('renders with the correct classes', () => {
+        const { container } = render(<LedLight color="red" glow={true} shine={true} />);
+
+        // Check for the presence of 'Led' and 'Glow' classes
+        const glowDiv = container.querySelector('.Glow');
+        expect(glowDiv).toBeInTheDocument();
+        expect(glowDiv).toHaveClass('Glow');
+
+        // Check for the 'Led' class and style
+        const ledDiv = container.querySelector('.Led');
+        expect(ledDiv).toBeInTheDocument();
+        expect(ledDiv).toHaveClass('Led');
+        expect(ledDiv).toHaveStyle('background-color: red');
+
+        // Check for 'Shine' class
+        const shineDiv = container.querySelector('.Shine');
+        expect(shineDiv).toBeInTheDocument();
     });
 
-    test('renders with specified color green', () => {
-        const { container } = render(<LedLight color="green" />);
-        expect(container.firstChild).toHaveClass('Led--green');
+    it('does not render Shine when shine prop is false', () => {
+        const { container } = render(<LedLight shine={false} />);
+        const shineDiv = container.querySelector('.Shine');
+        expect(shineDiv).not.toBeInTheDocument();
     });
 
-    test('renders with specified color orange', () => {
-        const { container } = render(<LedLight color="orange" />);
-        expect(container.firstChild).toHaveClass('Led--orange');
+    it('does not apply Glow class when glow prop is false', () => {
+        const { container } = render(<LedLight glow={false} />);
+        const glowDiv = container.querySelector('.Glow');
+        expect(glowDiv).not.toBeInTheDocument();
     });
 
-    test('renders with specified color red', () => {
-        const { container } = render(<LedLight color="red" />);
-        expect(container.firstChild).toHaveClass('Led--red');
+    it('has the corresponding styles when glow and shine props are true, and the color is red', () => {
+        const { container } = render(<LedLight color="red" glow={true} shine={true} />);
+
+        // Check for the 'Glow' class and glow effect style
+        const glowDiv = container.querySelector('.Glow');
+        expect(glowDiv).toBeInTheDocument();
+        expect(glowDiv).toHaveClass('Glow');
+        expect(glowDiv).toHaveStyle('box-shadow: 0 0 8px red');
+
+        // Check for the 'Led' class and color style
+        const ledDiv = glowDiv?.firstChild;
+        expect(ledDiv).toHaveClass(`Led`);
+        expect(ledDiv).toHaveStyle('background-color: red');
+
+        // Check for the 'Shine' class presence within the component
+        const shineDiv = container.querySelector('.Shine');
+        expect(shineDiv).toBeInTheDocument();
     });
 
-    test('renders default orange with a not supported specified color blue', () => {
-        const { container } = render(<LedLight color="blue" />);
-        expect(container.firstChild).toHaveClass('Led--blue');
+    it('has the corresponding styles when glow and shine props present (true), and the color is #D900FFFF', () => {
+        const { container } = render(<LedLight color="#D900FFFF" glow shine />);
+
+        // Check for the 'Glow' class and glow effect style
+        const glowDiv = container.querySelector('.Glow');
+        expect(glowDiv).toBeInTheDocument();
+        expect(glowDiv).toHaveClass('Glow');
+        expect(glowDiv).toHaveStyle('box-shadow: 0 0 8px #D900FFFF');
+
+        // Check for the 'Led' class and color style
+        const ledDiv = glowDiv?.firstChild;
+        expect(ledDiv).toHaveClass(`Led`);
+        expect(ledDiv).toHaveStyle('background-color: rgb(217, 0, 255)');
+
+        // Check for the 'Shine' class presence within the component
+        const shineDiv = container.querySelector('.Shine');
+        expect(shineDiv).toBeInTheDocument();
     });
 });
--- src/LedLight.tsx
+++ src/LedLight.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
 import '../styles/LedLight.css';
+
+/**
+ * Generate the box-shadow property for the specified color
+ * @param color {string} - the color to generate the glow effect with
+ */
+const generateGlow = (color: string = '#FFFFFF') => {
+    return `0 0 8px ${color}`;
+};
 
 interface LedLightProps {
     color?: string;
+    glow?: boolean;
+    shine?: boolean;
 }
 
 /**
  * LedLight
  *
- * @param color {string} - the color of the led light:
- *  - 'green'
- *  - 'orange'
- *  - 'red'
+ * @param color {string} - the color of the LED light
+ * @param glow {boolean} - specify if the led light will have a glow effect
+ * @param shine {boolean} - specify if the led light will have a shine shading on
  */
-export default function LedLight({color = 'orange'}:LedLightProps) {
-    return <div className={`Led Led--${color}`}></div>;
+export default function LedLight({color = 'orange', glow = false, shine = false}: LedLightProps) {
+    return <div
+        className={glow ? 'Glow' : ''}
+        style={{boxShadow: glow ? generateGlow(color) as string : 'none'}}
+    >
+        <div
+            className={`Led`}
+            style={{backgroundColor: color}}
+        ></div>
+        {shine && <div className="Shine"></div>}
+    </div>;
 }
